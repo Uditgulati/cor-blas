b070b60cf7d8b9544110e98dc7f17520ff5e59a1
uditgulati <uditgulati0@gmail.com>
2019-12-21 21:44:48 +0530

    pearson vecvec example working

--- examples/corTest/source/app.d
+++ examples/corTest/source/app.d
@@ -3,6 +3,8 @@ import std.stdio;
 
 
 void main() {
-  double[] A = [1, 2];
-  double val = pearson_vecvec(A.ptr, A.ptr, 2);
+  double[] A = [1, 2, 3, 1];
+  double[] B = [4, 1, 2, 10];
+  double val = pearson_vecvec(A.ptr, B.ptr, 4);
+  writeln(val);
 }
