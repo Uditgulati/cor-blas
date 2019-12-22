21462495b326eb86fe390ac9c28d64faff502a4c
uditgulati <uditgulati0@gmail.com>
2019-12-22 20:51:56 +0530

    write initial working spearman

--- source/cor/spearman.d
+++ source/cor/spearman.d
@@ -1,3 +1,39 @@
 module cor.spearman;
 
+import cblas : dot, syrk, Transpose, Order, Uplo;
+import std.stdio;
+import std.math : sqrt;
 
+import cor.utils : mean, rank;
+
+double spearman_vecvec(const double* x, const double* y, const int n) {
+  double[] copy_x = new double[n];
+  double[] copy_y = new double[n];
+
+  for(int i = 0; i < n; i++) {
+    copy_x[i] = x[i];
+    copy_y[i] = y[i];
+  }
+
+  double* rank_x = rank(copy_x.ptr, n);
+  double* rank_y = rank(copy_y.ptr, n);
+
+  double meanx = mean(rank_x, n);
+  double meany = mean(rank_y, n);
+
+  //calc minusmean_x, minusmean_y
+  for(int i = 0; i < n; i++) {
+    *(rank_x + i) -= meanx;
+    *(rank_y + i) -= meany;
+  }
+
+  //calc dot product
+  double dp = dot(n, rank_x, 1, rank_y, 1);
+
+  //calc normx & normy
+  double normx = dot(n, rank_x, 1, rank_x, 1);
+  double normy = dot(n, rank_y, 1, rank_y, 1);
+
+  double cor = dp / sqrt(normx * normy);
+  return cor;
+}
--- source/cor/utils.d
+++ source/cor/utils.d
@@ -1,5 +1,9 @@
 module cor.utils;
 
+import std.stdio;
+import std.algorithm.sorting;
+import std.typecons;
+
 double mean(const double* x, const int n) {
   double sum = 0.0;
 
@@ -8,4 +12,22 @@ double mean(const double* x, const int n) {
   }
 
   return (sum / cast(double)n);
+}
+
+double* rank(double* x, const int n) {
+  Tuple!(double, int)[] index_pairs = new Tuple!(double, int)[n];
+  for(int i = 0; i <  n; i++) {
+    index_pairs[i] = tuple(x[i], i);
+  }
+
+  index_pairs.sort();
+
+  double[] ranks = new double[n];
+
+  for(int i = 0 ; i < n; i++) {
+    int index = index_pairs[i][1];
+    ranks[index] = i + 1;
+  }
+
+  return ranks.ptr;
 }
